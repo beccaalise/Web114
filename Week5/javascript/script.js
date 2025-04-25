bcbc368b90a468cbd2267d0c7962554ff87e69aa
beccaalise <beccaacathey@gmail.com>
2025-04-25 02:00:56 -0500

    Create script.js

---
+++ Week5/javascript/script.js
@@ -0,0 +1,19 @@
+/* 
+  Author: Rebecca Cathey
+  Date: April 23, 2025
+*/
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Loading slot " + i + "...");
+  
+    if (i === 3) {
+      console.log("Dispensing: Cola");
+    } else if (i === 4) {
+      console.log("Dispensing: Sprite");
+    } else if (i === 5) {
+      console.log("Dispensing: Orange Juice");
+    } else {
+      console.log("Dispensing: Water");
+    }
+  }
