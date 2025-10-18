5c2a23707e25389649fe705eca39a3ee855de4a3
beccaalise <beccaacathey@gmail.com>
2025-10-17 19:11:07 -0500

    Create Budget.js
    Created Budget.js and completed the first couple steps

---
+++ Week2/Budget.js
@@ -0,0 +1,18 @@
+//Web114: Week 2 - Budget Activity
+//Author: Rebecca Cathey
+//Date: October 17th 2025
+
+/*Monthly Variables*/
+let money = 4000;
+let housing = 1200;
+let food = 300;
+let monthlyUtilities = 150;
+
+/*Calculate Total Income*/
+let totalIncome = money;
+
+/*Calculate Total Expenses*/
+let totalExpenses = housing + food +monthlyUtilities;
+
+/*Calculate Remaining Money*/
+let remainingMoney = totalIncome - totalExpenses;
